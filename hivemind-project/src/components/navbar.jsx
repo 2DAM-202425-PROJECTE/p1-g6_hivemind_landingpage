6a6f49dd3a71279ec5af021e40cddfeaecbb6561
harrywhite17 <harrywhite@iesebre.com>
2024-11-15 19:34:25 +0100

    FixProfile

--- hivemind-project/src/components/navbar.jsx
+++ hivemind-project/src/components/navbar.jsx
@@ -97,28 +97,20 @@ export default function NavigationBar() {
                                     <span className="sr-only">Open user menu</span>
                                     <img
                                         alt="User profile"
-                                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                        src="https://i.pinimg.com/originals/ac/11/aa/ac11aa2add3b0193c8769e0a17d13535.jpg"
                                         className="h-8 w-8 rounded-full"
                                     />
                                 </MenuButton>
                             </div>
-                            <MenuItems className="absolute right-0 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5">
-                                <MenuItem>
-                                    <a href="/profile" className="block px-4 py-2 text-sm text-gray-700">
-                                        Your Profile
-                                    </a>
+                            <MenuItems className="absolute right-0 mt-2 w-48 origin-top-right rounded-md bg-black py-1 shadow-lg ring-1 ring-white ring-opacity-5">
+                              <MenuItem>
+                                 <a href="/profile" className="block px-4 py-2 text-sm text-white">Your Profile</a>
+                              </MenuItem>
+                            <MenuItem><a href="#" className="block px-4 py-2 text-sm text-white">Settings</a>
                                 </MenuItem>
-                                <MenuItem>
-                                    <a href="#" className="block px-4 py-2 text-sm text-gray-700">
-                                        Settings
-                                    </a>
+                             <MenuItem><a href="#" className="block px-4 py-2 text-sm text-white">Sign out</a>
                                 </MenuItem>
-                                <MenuItem>
-                                    <a href="#" className="block px-4 py-2 text-sm text-gray-700">
-                                        Sign out
-                                    </a>
-                                </MenuItem>
-                            </MenuItems>
+<                           /MenuItems>
                         </Menu>
                     </div>
                 </div>
