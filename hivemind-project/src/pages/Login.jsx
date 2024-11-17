b82de2835a6c7afa3786bdd816d2a75e979ca506
manelmendez1 <manelmendez@iesebre.com>
2024-11-17 17:59:29 +0100

    Gestió de l'estat dels camps del formulari i la lògica d'enviament del formulari

--- hivemind-project/src/pages/Login.jsx
+++ hivemind-project/src/pages/Login.jsx
@@ -1,14 +1,26 @@
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import axios from "axios";
+
 const Login = () => {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const navegate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios.post('http://localhost:3001/login', {username, password})
+            .then(result => {
+                console.log(result);
+                navegate('/dashboard');
+            })
+            .catch(err => console.log(err));
+    };
+
+
     return (
         <>
-          {/*
-            This example requires updating your template:
-    
-            ```
-            <html class="h-full bg-white">
-            <body class="h-full">
-            ```
-          */}
           <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
               <img
