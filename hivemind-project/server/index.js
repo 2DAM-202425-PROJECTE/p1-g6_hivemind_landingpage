731c5e7cddb6850b7a8d4eae76e0144505cc9ffa
manelmendez1 <manelmendez@iesebre.com>
2024-11-17 18:58:40 +0100

    Gestió de l'inici de sessió

--- hivemind-project/server/index.js
+++ hivemind-project/server/index.js
@@ -7,13 +7,28 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-mongoose.connect("mongodb://dasrado.com:27017/hivemind");
+mongoose.connect("mongodb://admin:5kh3gbfwdrwc@dasrado.com:27017/hivemind?authSource=admin");
+
+
 
 app.post('/register', (req, res) => {
     HivemindModel.create(req.body)
         .then(hiveminds => res.json(hiveminds))
         .catch(err => res.json(err))
 })
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    HivemindModel.findOne({ username, password })
+        .then(user => {
+            if (user) {
+                res.json({ message: "Login successful", user });
+            } else {
+                res.status(401).json({ message: "Invalid credentials" });
+            }
+        })
+        .catch(err => res.json(err));
+});
 
 app.listen(3001, () => {
     console.log("server is running")
